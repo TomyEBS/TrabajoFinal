9c8cbf8bae143c5b68b0486643cc82a22594f09d
Tomas Breglia <bregliatomas4@gmail.com>
2023-04-21 22:05:39 -0300

    Primera subida

--- src/components/pages/Tienda.js
+++ src/components/pages/Tienda.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React , {useState} from "react";
+import { UseFetch } from "../UseFetch";
+import {Cards} from '../Cards/Cards'
+
 
 const Tienda = () => {
+    const [ url, setUrl]=useState('https://pokeapi.co/api/v2/pokemon') 
+    const estado = UseFetch(url)
+    const {cargando,data}=estado
+    cargando? console.log('cargando'):console.log(data.results)
+
+
+
     return (
        <div>
          <h1>Esta es la tienda</h1>
+
+      {
+        cargando
+        ?
+        <h1>Cargando. . .</h1>
+        :
+       <div>
+        <Cards results={data.results}/>
+          <div className='container m-auto'>
+            <button onClick={()=>setUrl(data.previous)} className='m-2 btn btn-dark'>Anterior</button>
+            <button onClick={()=>setUrl(data.next)}  className='m-2 btn btn-dark'>Siguiente</button>
+          </div>
+
+        </div>
+        }
+
+       
        </div>
+
     )
 
 }
