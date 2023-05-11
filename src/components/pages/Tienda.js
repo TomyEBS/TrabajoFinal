c9f99615bd63823999c9c91b4690189cd860706c
Tomas Breglia <bregliatomas4@gmail.com>
2023-05-11 18:48:10 -0300

    Cambio de página

--- src/components/pages/Tienda.js
+++ src/components/pages/Tienda.js
@@ -1,40 +1,52 @@
-import React , {useState} from "react";
+import React , {useEffect, useState} from "react";
 import { UseFetch } from "../UseFetch";
 import {Cards} from '../Cards/Cards'
 
 
 const Tienda = () => {
-    const [ url, setUrl]=useState('https://pokeapi.co/api/v2/pokemon') 
-    const estado = UseFetch(url)
-    const {cargando,data}=estado
-    cargando? console.log('cargando'):console.log(data.results)
+    const [productos, setProductos] = useState([]);
+
+  React.useEffect(() => {
+    traerProductos();
+  }, []);
+
+async function traerProductos(){
+  const url = 'https://fakestoreapi.com/products'
+
+  
+  try {
+    const resultado = await fetch(url)
+    const respuesta = await resultado.json()
+    setProductos(respuesta);
 
 
+  } catch(error){
+    console.log(error)
+  }
 
-    return (
-       <div>
-         <h1>Esta es la tienda</h1>
+  
 
-      {
-        cargando
-        ?
-        <h1>Cargando. . .</h1>
-        :
-       <div>
-        <Cards results={data.results}/>
-          <div className='container m-auto'>
-            <button onClick={()=>setUrl(data.previous)} className='m-2 btn btn-dark'>Anterior</button>
-            <button onClick={()=>setUrl(data.next)}  className='m-2 btn btn-dark'>Siguiente</button>
-          </div>
-
-        </div>
-        }
-
-       
-       </div>
-
-    )
 
 }
+return (
+  <div>
+    {productos.map((prod) => (
+      <div className="card" style={{ width: '18rem' }} key={prod.id}>
+        <img className="card-img-top" src={prod.image} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{prod.title}</h5>
+          <p className="card-text">{prod.description}</p>
+          <p className="card-subtitle mb-2 text-muted">{prod.price}</p>
+          <p className="card-subtitle mb-2 ">{prod.category}</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+};
+
 
 export default Tienda;
